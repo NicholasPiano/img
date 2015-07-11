d010ece3f9ebcf42963336c73a045fd640928b43
NicholasPiano <nicholas.d.piano@gmail.com>
2015-07-11 02:10:33 +0100

    running pipeline

--- woot/apps/expt/management/commands/step02_zmod.py
+++ woot/apps/expt/management/commands/step02_zmod.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
     make_option('--expt', # option that will appear in cmd
       action='store', # no idea
       dest='expt', # refer to this in options variable
-      default='050714', # some default
+      default='050714-test', # some default
       help='Name of the experiment to import' # who cares
     ),
 
